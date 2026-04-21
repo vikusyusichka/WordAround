77ecfa349e24047da492d15a03b4f8b5bae69db0
vikusyusichka <vikusyusichka@gmail.com>
2026-04-21 21:57:56 +0300

    Initial Commit

---
+++ WordAround/WordAroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordAroundApp.swift
+//  WordAround
+//
+//  Created by vikusyusichka on 21.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct WordAroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
